29005ce086093715c0e8fe00f84ba5eca8bcbf87
Rick Byers <rick-biojava@rbyers.net>
2008-09-10 21:08:39 +0000

    Removed -log_folder arg - want to see error output.
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/trunk@5872 7c6358e6-4a41-0410-a743-a5b2a554c398

--- RCSB Viewer Jars/TestRuns/lxtest.bat
+++ RCSB Viewer Jars/TestRuns/lxtest.bat
@@ -1,1 +1,1 @@
-java -D"java.library.path=../3rdParty/jnilibs" -jar ../RCSBViewers/RCSB-LigandExplorer.jar -standalone -log_folder ./temp -structure_url molecules/1x7b.xml.gz -ligand 041
+java -D"java.library.path=../3rdParty/jnilibs" -jar ../RCSBViewers/RCSB-LigandExplorer.jar -standalone -structure_url molecules/1x7b.xml.gz -ligand 041
--- RCSB Viewer Jars/TestRuns/pwtest.bat
+++ RCSB Viewer Jars/TestRuns/pwtest.bat
@@ -1,3 +1,3 @@
 set MOL=1STP.xml.gz
 if NOT "%1" == "" set MOL=%1
-java -D"java.library.path=../3rdParty/jnilibs" -jar ../RCSBViewers/RCSB-ProteinWorkshop.jar -standalone -log_folder ./temp -structure_url molecules/%MOL% %2
+java -D"java.library.path=../3rdParty/jnilibs" -jar ../RCSBViewers/RCSB-ProteinWorkshop.jar -standalone -structure_url molecules/%MOL% %2
