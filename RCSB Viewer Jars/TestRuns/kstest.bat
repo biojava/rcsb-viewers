d8e28dc11b3edde744dfca6de83d991829efcaf3
Peter Rose <pwrose@ucsd.edu>
2009-02-12 01:07:17 +0000

    PDBWW-1538 Updated path to RCSB-Kiosk.jar file which is now in the RCSBViewers folder.
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/trunk@6800 7c6358e6-4a41-0410-a743-a5b2a554c398

--- RCSB Viewer Jars/TestRuns/kstest.bat
+++ RCSB Viewer Jars/TestRuns/kstest.bat
@@ -1,3 +1,4 @@
 call setargs.bat
 rem call debugargs.bat
-java %DEBUGARGS% %PATHARGS% -jar ../RCSBKiosk/RCSB-Kiosk.jar -show_asymmetric_unit_only
+rem Displays assymetric unit of molecules
+java %DEBUGARGS% %PATHARGS% -jar ../RCSBViewers/RCSB-Kiosk.jar -show_asymmetric_unit_only
