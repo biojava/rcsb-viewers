e66f858853e6f1bc4f87127aaa7d7b06d8a4191a
Peter Rose <pwrose@ucsd.edu>
2009-06-16 21:50:20 +0000

    PDBWW-1809, PDBWW-1813, PDBWW-1871, PDBWW-1917, PDBWW-1969, PDBWW-1989, PDBWW-1990 bug fixes and feature additions.
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/trunk@6971 7c6358e6-4a41-0410-a743-a5b2a554c398

--- RCSB Ligand Explorer/Structure Model/org/rcsb/lx/model/LXModel.java
+++ RCSB Ligand Explorer/Structure Model/org/rcsb/lx/model/LXModel.java
@@ -47,9 +47,7 @@ package org.rcsb.lx.model;
 
 import org.rcsb.lx.ui.ContactMap;
 import org.rcsb.mbt.model.Atom;
-import org.rcsb.mbt.model.StructureModel;
 import org.rcsb.mbt.model.Residue;
-import org.rcsb.mbt.model.util.PdbToNdbConverter;
 import org.rcsb.mbt.model.util.IResidueNameModifier;
 import org.rcsb.uiApp.model.UIAppStructureModel;
 
@@ -58,59 +56,34 @@ public class LXModel extends UIAppStructureModel implements IResidueNameModifier
 {
 	private String initialLigand = null;	
 	private final InteractionMap interactionMap = new InteractionMap();
-    private ContactMap contactMap = null;
-    
-    @Override
-    public void clear()
-    {
-    	super.clear();
-    	initialLigand = null;
-    }
-    
-    /**
-     * Gets the residue name and id from the residue and atom.  Used primarily in the
-     * ligandJList in the sidebar
-     * 
-     * @param atom
-     * @return
-     */
+	private ContactMap contactMap = null;
+
+	@Override
+	public void clear()
+	{
+		super.clear();
+		initialLigand = null;
+	}
+
+	/**
+	 * Gets the residue name and id from the residue and atom.  Used primarily in the
+	 * ligandJList in the sidebar
+	 * 
+	 * @param atom
+	 * @return
+	 */
 	public String getResidueName(final Atom atom)
 	{
-		final PdbToNdbConverter converter = atom.structure.getStructureMap().getPdbToNdbConverter();
-		final Object[] tmp = converter.getPdbIds(atom.chain_id, new Integer(atom.residue_id));
-		String name = "";
-		
-		if (tmp == null)
-		{
-			// use ndb ids
-			name = atom.chain_id;
-			if (name.length() > 0)
-				name += " " + atom.residue_id;
-			
-			else
-				name = atom.residue_id + "";
+		String name = atom.authorChain_id;
 
-		}
-		
+		if (name.length() > 0)
+			name += " " + atom.authorResidue_id;		
 		else
-		{
-			// use pdb ids
-			if (tmp[0] != null)
-				name = (String) tmp[0];
+			name = String.valueOf(atom.authorResidue_id);
 
-			if (tmp[1] != null)
-			{
-				if (name.length() > 0)
-					name += " " + tmp[1];
-				
-				else
-					name = (String) tmp[1];
-			}
-		}
 
 		if (name.length() > 0)
 			name = atom.compound + " (" + name + ")";
-		
 		else
 			name = atom.compound;
 
@@ -128,7 +101,7 @@ public class LXModel extends UIAppStructureModel implements IResidueNameModifier
 	}
 
 
-	
+
 	public InteractionMap getInteractionMap()
 	{
 		return this.interactionMap;
