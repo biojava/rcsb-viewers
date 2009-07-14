0e6a577618cc08348e1ee6e7307d2acae32b0fba
Peter Rose <pwrose@ucsd.edu>
2009-07-14 22:53:41 +0000

    PDBWW-2006 Fixed display problem with distance line. Structure was not assigned to line segment.
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/trunk@7011 7c6358e6-4a41-0410-a743-a5b2a554c398

--- RCSB Protein Workshop/Controllers/org/rcsb/pw/controllers/scene/mutators/LinesMutator.java
+++ RCSB Protein Workshop/Controllers/org/rcsb/pw/controllers/scene/mutators/LinesMutator.java
@@ -46,6 +46,7 @@
 package org.rcsb.pw.controllers.scene.mutators;
 
 import java.awt.Color;
+import java.util.Arrays;
 import java.util.Vector;
 
 import org.rcsb.mbt.model.Atom;
@@ -134,6 +135,7 @@ public class LinesMutator extends MutatorBase
 		final StructureMap sm = AppBase.sgetModel().getStructures().get(0).getStructureMap();
 		sm.getStructureStyles().setStyle(line, style);
 		System.arraycopy(this.getOptions().getColor(), 0, style.getColor(), 0, style.getColor().length);
+		line.setStructure(sm.getStructure());
 		((JoglSceneNode)sm.getUData()).addRenderable(new DisplayListRenderable( line, style, geometry ));
 
 		lines.add(line);
