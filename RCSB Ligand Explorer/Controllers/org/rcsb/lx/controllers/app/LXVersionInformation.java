fbe8a9273e0b584f4933ba7a44ba17a70a3b7578
Peter Rose <pwrose@ucsd.edu>
2009-09-08 21:59:32 +0000

    Updated version number from 3.5 to 3.6
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/trunk@7212 7c6358e6-4a41-0410-a743-a5b2a554c398

--- RCSB Ligand Explorer/Controllers/org/rcsb/lx/controllers/app/LXVersionInformation.java
+++ RCSB Ligand Explorer/Controllers/org/rcsb/lx/controllers/app/LXVersionInformation.java
@@ -54,5 +54,5 @@ public class LXVersionInformation extends VersionInformation
 	 * 
 	 * @return - version string
 	 */
-	public static String version() { return "3.5"; }
+	public static String version() { return "3.6"; }
 }
