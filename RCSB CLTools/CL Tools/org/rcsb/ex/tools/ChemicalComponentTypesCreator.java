a64cae0d138230f55a6ae791024ab2dbd1a1c0e2
Peter Rose <pwrose@ucsd.edu>
2010-02-05 01:26:22 +0000

    Fix for PDBWW-2774, added new infrastructure to use chemical component type information from Chemical Component Dictionary
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/trunk@7451 7c6358e6-4a41-0410-a743-a5b2a554c398

--- RCSB CLTools/CL Tools/org/rcsb/ex/tools/ChemicalComponentTypesCreator.java
+++ RCSB CLTools/CL Tools/org/rcsb/ex/tools/ChemicalComponentTypesCreator.java
@@ -55,6 +55,9 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
 /**
@@ -63,17 +66,24 @@ import java.util.zip.GZIPInputStream;
  * @author rickb
  *
  */
-public class FindAllNucleicAcidCompoundNames {
+public class ChemicalComponentTypesCreator {
 	public static final String ftpInputPath = "ftp://ftp.wwpdb.org/pub/pdb/data/monomers/components.cif.gz";
-	public static final String defaultOutputPath = "NucleicAcidCompoundNames.dat";
-	
+	public static final String defaultOutputPath = "ChemicalComponentTypes.tsv";
+
 	public static void main(String[] args)
 	{
 		String outputPath = defaultOutputPath;
 		BufferedReader in = null;
 		PrintWriter out = null;
 		String connectedMsg;
-		
+
+		Set<String> types = new HashSet<String>();
+		types.add("L-PEPTIDE LINKING");
+		types.add("DNA LINKING");
+		types.add("RNA LINKING");
+
+		Set<String> uniqueTypes = new HashSet<String>();
+
 		try {
 			InputStream inputStream = null;
 
@@ -82,12 +92,12 @@ public class FindAllNucleicAcidCompoundNames {
 				System.out.println("Connecting to ftp site...");
 				URL ComponentBondsUrl = new URL(ftpInputPath);
 				URLConnection urlConnection = ComponentBondsUrl.openConnection();
-		        urlConnection.addRequestProperty("User-agent", "Mozilla/4.0 (compatible; MSIE 6.0;Windows NT 5.1; SV1)");
+				urlConnection.addRequestProperty("User-agent", "Mozilla/4.0 (compatible; MSIE 6.0;Windows NT 5.1; SV1)");
 				urlConnection.setReadTimeout(10000);
 				inputStream = urlConnection.getInputStream();
-				connectedMsg = "Connected, streaming nucleotide information...";
+				connectedMsg = "Connected, streaming modified residue information...";
 			}
-			
+
 			else
 			{
 				System.out.println("Opening file: " + args[0]);
@@ -101,55 +111,87 @@ public class FindAllNucleicAcidCompoundNames {
 			InputStreamReader isReader = new InputStreamReader(gzipInputStream);
 			in = new BufferedReader(isReader);
 			out = new PrintWriter(new FileWriter(new File(outputPath)));
-			
+
 			String curChemCompId = null;
-			
+			String type = null;
+
 			String line = null;
-			int lineCount = 0, lineEvery = 10;
-			
+
 			while((line = in.readLine()) != null) {
 				line = line.trim();
-				
+
 				if(line.startsWith("_chem_comp.id")) {
+					curChemCompId = null;
+					type = null;
 					String[] split = line.split("\\s++");
 					if(split == null || split.length != 2) {
 						new Exception("unexpected _chem_comp.id value").printStackTrace();
 						continue;
 					}
-					
-					curChemCompId = split[1];
-					if(curChemCompId.charAt(0) == '"' || curChemCompId.charAt(curChemCompId.length() - 1) == '"') {
-						curChemCompId = curChemCompId.substring(1, curChemCompId.length() - 1);
-					}
+
+					curChemCompId = stripQuotes(split[1]);			
 				}
-				
+
 				else if (line.startsWith("_chem_comp.type"))
 				{
-					String type = line.substring("_chem_comp.type".length()).trim();
+					type = line.substring(15).trim();
 					if(type == null || type.length() == 0)
 					{
 						new Exception("unexpected _chem_comp.id value").printStackTrace();
 						continue;
 					}
+
+					// case is inconsistently used in Chemical Component Dictionary, convert to upper case
+					type = type.toUpperCase();
+					type = stripQuotes(type);
 					
-					if(type.equals("\"DNA LINKING\"") || type.equals("\"RNA LINKING\""))
-					{
-						if ((++lineCount % lineEvery) == 1)
-							System.out.println(lineCount + ": " + type + " - " + curChemCompId);
-						out.println(curChemCompId);
+					// peptide linking is inconsistently used in Chemical Component Dictionary
+					// filed as issue: http://tracking.wwpdb.org/jira/browse/DATAQUALITY-39
+					if (type.equals("PEPTIDE-LINKING")) {
+						type = "PEPTIDE LINKING";
+					}
+					uniqueTypes.add(type);
+					System.out.println(type);
+				}
+				else if (line.startsWith("_chem_comp.mon_nstd_parent_comp_id"))
+				{
+					String nstdParent = line.substring(34).trim();
+					if (nstdParent == null || nstdParent.length() == 0) {
+						new Exception("unexpected _chem_comp.id value").printStackTrace();
+					}
+
+					nstdParent = stripQuotes(nstdParent);
+					if (type.equals("NON-POLYMER")) {
+						continue;
+					}
+					if (nstdParent.equals("?")) {
+						out.println(curChemCompId + "\t" + type);
+					} else {
+
+						String[] token = nstdParent.split(",");
+						System.out.println(Arrays.toString(token));
+
+						if (type != null) {
+							out.print(curChemCompId + "\t" + type);
+							for (String name: token) {
+								out.print("\t" + name.trim());
+							}
+							out.println();
+						}
 					}
 				}
 			}
+			System.out.println("Types: " + uniqueTypes);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		finally
 		{
 			try
 			{
 				System.out.println("Done.");
-				
+
 				if (in != null)
 					in.close();
 
@@ -157,12 +199,19 @@ public class FindAllNucleicAcidCompoundNames {
 					out.close();
 
 			}
-			
+
 			catch (IOException e)
 			{
 				e.printStackTrace();
 			}
 		}
-		
+
+	}
+
+	private static String stripQuotes(String string) {
+		if (string.startsWith("\"") && string.endsWith("\"")) {
+			return string.substring(1, string.length()-1);
+		}
+		return string;
 	}
 }
