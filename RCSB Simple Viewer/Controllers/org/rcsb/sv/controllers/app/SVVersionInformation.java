c9d73a17bfebe59ce0bff75ce9c2ef2b73bdcf43
Peter Rose <pwrose@ucsd.edu>
2010-04-13 18:51:43 +0000

    Updated version to 3.8
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/trunk@7677 7c6358e6-4a41-0410-a743-a5b2a554c398

--- RCSB Simple Viewer/Controllers/org/rcsb/sv/controllers/app/SVVersionInformation.java
+++ RCSB Simple Viewer/Controllers/org/rcsb/sv/controllers/app/SVVersionInformation.java
@@ -49,5 +49,5 @@ import org.rcsb.uiApp.controllers.app.VersionInformation;
 
 public class SVVersionInformation extends VersionInformation
 {
-	public static String version() { return "3.7"; }
+	public static String version() { return "3.8"; }
 }
