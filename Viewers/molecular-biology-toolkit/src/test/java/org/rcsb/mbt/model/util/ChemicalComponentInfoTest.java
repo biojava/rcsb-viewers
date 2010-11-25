3e28fa1ce84624254697d01dadbc076456149a4e
Andreas Prlic <andreas.prlic@gmail.com>
2010-11-25 22:33:10 +0000

    new test for correct packaging
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/maven@8543 7c6358e6-4a41-0410-a743-a5b2a554c398

--- Viewers/molecular-biology-toolkit/src/test/java/org/rcsb/mbt/model/util/ChemicalComponentInfoTest.java
+++ Viewers/molecular-biology-toolkit/src/test/java/org/rcsb/mbt/model/util/ChemicalComponentInfoTest.java
@@ -1,17 +1,17 @@
-import org.rcsb.mbt.model.util.ChemicalComponentInfo;
+package org.rcsb.mbt.model.util;
 
+import org.rcsb.mbt.model.util.ChemicalComponentInfo;
 import junit.framework.TestCase;
 
 
-public class ChemCompInfoTest  extends TestCase{
+public class ChemicalComponentInfoTest  extends TestCase{
+
 
 	public void  testChemCompInfo(){
-
-		
-		
 		String letter = ChemicalComponentInfo.getLetterFromCode("ALA");
-		
+
 		assertNotNull("got a null instead of A", letter);
 		assertEquals("Is not A!", letter,"A");
+
 	}
 }
