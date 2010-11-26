d15d987892857c8d1c2df0fe82b421440c49fcc0
Andreas Prlic <andreas.prlic@gmail.com>
2010-11-26 18:06:25 +0000

    removing com.sun.imageio.plugins.jpeg.JPEGImageWriter import
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/maven/Viewers@8550 7c6358e6-4a41-0410-a743-a5b2a554c398

--- framework/src/main/java/org/rcsb/uiApp/ui/dialogs/ImageFileSaver.java
+++ framework/src/main/java/org/rcsb/uiApp/ui/dialogs/ImageFileSaver.java
@@ -50,16 +50,18 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriter;
 import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
 import javax.imageio.stream.ImageOutputStream;
 
 import org.w3c.dom.Element;
 
-import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
+//import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
 import com.sun.media.jai.codec.ImageCodec;
 import com.sun.media.jai.codec.ImageEncoder;
 import com.sun.media.jai.codec.PNGEncodeParam;
@@ -132,7 +134,7 @@ public final class ImageFileSaver {
 	private static void saveJPEG(final BufferedImage image, final int dpi, final String filename) throws IOException
 	{	
 		// get image writer 
-        JPEGImageWriter imageWriter = (JPEGImageWriter) ImageIO.getImageWritersBySuffix(ImageFileFormat.JPEG.name()).next();
+        ImageWriter imageWriter =  ImageIO.getImageWritersBySuffix(ImageFileFormat.JPEG.name()).next();
         ImageOutputStream ios = ImageIO.createImageOutputStream(new File(filename));
         imageWriter.setOutput(ios);
 
