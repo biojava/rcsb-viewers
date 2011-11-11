e7bc6d8fca97e085358272bb7f20351f1b6e940d
Andreas Prlic <andreas.prlic@gmail.com>
2011-11-11 23:58:14 +0000

    moving Demo to demo package
    git-svn-id: svn+ssh://dev.open-bio.org/home/svn-repositories/biojava/RCSB Viewers/maven/Viewers@9415 7c6358e6-4a41-0410-a743-a5b2a554c398

---
+++ applications/src/main/java/demo/Demo.java
@@ -0,0 +1,40 @@
+package demo;
+
+import org.rcsb.ks.controllers.app.SlideShow;
+import org.rcsb.lx.controllers.app.LigandExplorer;
+import org.rcsb.pw.controllers.app.ProteinWorkshop;
+import org.rcsb.sv.controllers.app.SimpleViewer;
+
+public class Demo {
+
+	public static void main(String[] args){
+		//showSimpleViewer();
+		showProteinWorkshop();
+		//showLigandExplorer();
+		//showKiosk();
+	}
+
+	public static void showSimpleViewer(){
+		final SimpleViewer app = new SimpleViewer(null);
+		app.initialize(true, true);
+	}
+	
+	public static void showProteinWorkshop(){
+		final ProteinWorkshop app = new ProteinWorkshop(null);	
+		app.initialize(true, true);
+	}
+	
+	public static void showLigandExplorer() {
+		final LigandExplorer app = new LigandExplorer(null);		
+		app.initialize(true);
+		
+	}
+	
+	public static void showKiosk() {
+		
+		String[] pdbIds = new String[]{"-structure_id_list","4hhb,1cdg"};
+		SlideShow show = new SlideShow(pdbIds);
+		show.run();
+	}
+	
+}
